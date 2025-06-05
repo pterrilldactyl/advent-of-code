33692b44f7e5bd63246dbc42b4e5113d313518af
Terry Anderson <phoenix.tra3196@gmail.com>
2025-06-05 17:01:20 -0500

    Created area function

--- 2015/Day-2/day2.php
+++ 2015/Day-2/day2.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "fin";
+$input = file_get_contents("input.txt");
+
+function getPaperArea($x, $y, $z)
+{
+    $smallest = min($x, $y, $z);
+
+    $area_needed = 2*$smallest + 2*$x + 2*$y + 2*$z;
+
+    return $area_needed;
+}
+
+//echo getPaperArea(2,3,4);
