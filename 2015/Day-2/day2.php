4645486dca5e5aa945e4a64b0e586a3e030d232d
Terry Anderson <phoenix.tra3196@gmail.com>
2025-06-06 16:58:29 -0500

    Fixed my math and simplified some logic

--- 2015/Day-2/day2.php
+++ 2015/Day-2/day2.php
@@ -2,39 +2,40 @@
 
 $input = file_get_contents("input.txt");
 
+// Outputs an array delimited by new line character
+function changeInputFileToArray($input)
+{
+    $inputItemized = explode("\n", $input); // Splits the challenge input into an array of box dimensions by HxWxL
+
+    return $inputItemized;
+}
+
 function getPaperArea($boxdimensions)
 {
-    $x = $boxdimensions[0];
-    $y = $boxdimensions[1];
-    $z = $boxdimensions[2];
+    $dimensionParts = explode("x", $boxdimensions); // Splits the box dimensions into 3 separate units
+    sort($dimensionParts);
+    //$dimensionsSorted = sort($dimensionParts);
+
+    $x = $dimensionParts[0];
+    $y = $dimensionParts[1];
+    $z = $dimensionParts[2];
+
     $smallest = min($x, $y, $z);
 
-    //echo $x."\n".$y."\n".$z."\n";
-
-    $area_needed = 2*$smallest + 2*$x + 2*$y + 2*$z;
-
+    $area_needed = 3*$x*$y + 2*$y*$z + 2*$z*$x;
+    echo "This is Area Needed per box: " . $area_needed . "\n";
     return $area_needed;
 }
 
 
 
+$area = 0;
 
-$inputItemized = explode("\n", $input); // Splits the challenge input into an array of box dimensions by HxWxL
-$package = $inputItemized[0]; // $package is one box from the list
-$packageParts = explode("x", $package); // Splits the box dimensions into 3 separate units
-
-/*
-foreach ($inputItemized as $x)
+foreach (changeInputFileToArray($input) as $x)
 {
-    echo $x."!\n";
+    echo "This is x: ".$x."\n";
+    $area += getPaperArea($x);
+    echo "New Total Area: " . $area."\n";
 }
-*/
 
-$a = $packageParts[0]+$packageParts[1];
-//echo var_dump($inputItemized);
-//echo $package;
-//echo var_dump($packageParts);
-//echo "\n";
-//echo $a;
-//echo gettype($packageParts[0]*2);
-echo getPaperArea($packageParts);
+// Part one is done. Finally. Stupid math.
