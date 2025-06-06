bc933c8130106c362945323ea72e4c3bbf655ffc
Terry Anderson <phoenix.tra3196@gmail.com>
2025-06-06 15:55:58 -0500

    Adding more progress

--- 2015/Day-2/day2.php
+++ 2015/Day-2/day2.php
@@ -2,13 +2,39 @@
 
 $input = file_get_contents("input.txt");
 
-function getPaperArea($x, $y, $z)
+function getPaperArea($boxdimensions)
 {
+    $x = $boxdimensions[0];
+    $y = $boxdimensions[1];
+    $z = $boxdimensions[2];
     $smallest = min($x, $y, $z);
+
+    //echo $x."\n".$y."\n".$z."\n";
 
     $area_needed = 2*$smallest + 2*$x + 2*$y + 2*$z;
 
     return $area_needed;
 }
 
-//echo getPaperArea(2,3,4);
+
+
+
+$inputItemized = explode("\n", $input); // Splits the challenge input into an array of box dimensions by HxWxL
+$package = $inputItemized[0]; // $package is one box from the list
+$packageParts = explode("x", $package); // Splits the box dimensions into 3 separate units
+
+/*
+foreach ($inputItemized as $x)
+{
+    echo $x."!\n";
+}
+*/
+
+$a = $packageParts[0]+$packageParts[1];
+//echo var_dump($inputItemized);
+//echo $package;
+//echo var_dump($packageParts);
+//echo "\n";
+//echo $a;
+//echo gettype($packageParts[0]*2);
+echo getPaperArea($packageParts);
