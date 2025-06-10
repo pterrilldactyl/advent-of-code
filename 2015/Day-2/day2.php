e0bc413da16c405f896efc7681a130e3e627b83b
Terry Anderson <phoenix.tra3196@gmail.com>
2025-06-10 10:15:05 -0500

    Refactor

--- 2015/Day-2/day2.php
+++ 2015/Day-2/day2.php
@@ -5,37 +5,40 @@ $input = file_get_contents("input.txt");
 // Outputs an array delimited by new line character
 function changeInputFileToArray($input)
 {
-    $inputItemized = explode("\n", $input); // Splits the challenge input into an array of box dimensions by HxWxL
+    $input_itemized = explode("\n", $input); // Splits the challenge input into an array of box dimensions by HxWxL
 
-    return $inputItemized;
+    return $input_itemized;
 }
 
-function getPaperArea($boxdimensions)
+function getPaperArea($dimensions)
 {
-    $dimensionParts = explode("x", $boxdimensions); // Splits the box dimensions into 3 separate units
-    sort($dimensionParts);
-    //$dimensionsSorted = sort($dimensionParts);
+    $parts = explode("x", $dimensions); // Splits the box dimensions into 3 separate units
+    sort($parts);
 
-    $x = $dimensionParts[0];
-    $y = $dimensionParts[1];
-    $z = $dimensionParts[2];
+    $x = $parts[0];
+    $y = $parts[1];
+    $z = $parts[2];
 
     $smallest = min($x, $y, $z);
 
     $area_needed = 3*$x*$y + 2*$y*$z + 2*$z*$x;
-    echo "This is Area Needed per box: " . $area_needed . "\n";
+    //echo "This is Area Needed per box: " . $area_needed . "\n";
     return $area_needed;
 }
 
 
-
-$area = 0;
-
-foreach (changeInputFileToArray($input) as $x)
+function getTotalArea($input)
 {
-    echo "This is x: ".$x."\n";
-    $area += getPaperArea($x);
-    echo "New Total Area: " . $area."\n";
+    $area = 0;
+
+    foreach (changeInputFileToArray($input) as $x)
+    {
+        //echo "This is x: ".$x."\n";
+        $area += getPaperArea($x);
+        //echo "New Total Area: " . $area."\n";
+    }
+    return $area;
 }
 
+echo getTotalArea($input);
 // Part one is done. Finally. Stupid math.
